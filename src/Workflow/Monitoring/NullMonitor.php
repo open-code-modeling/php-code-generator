683657307a83c14d51fecc78f84617c5f450ce19
Sandro Keil <github@sandro-keil.de>
2020-10-05 23:36:13 +0200

    Add GitHub CI

--- src/Workflow/Monitoring/NullMonitor.php
+++ src/Workflow/Monitoring/NullMonitor.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * @see       https://github.com/open-code-modeling/php-code-generator for the canonical source repository
  * @copyright https://github.com/open-code-modeling/php-code-generator/blob/master/COPYRIGHT.md
  * @license   https://github.com/open-code-modeling/php-code-generator/blob/master/LICENSE.md MIT License
